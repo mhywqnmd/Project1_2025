f88a96334d738600a7dd91b8e7b95f0fbfe55818
mhywqnmd <3251995934@qq.com>
2025-04-11 20:48:02 +0800

    SunHaoxiang/202483890020/second task

--- reaction.py
+++ reaction.py
@@ -9,16 +9,34 @@ left_button = Button(14)
 left_name = input('left player name is ')
 right_name = input('right player name is ')
 
-led.on()
-sleep(uniform(5,10))
-led.off()
+left_score = 0
+right_score = 0
+
+rounds = 3
+
+for _ in range(rounds):
+	led.on()
+	sleep(uniform(5,10))
+	led.off()
 
 def pressed(button):
+	global left_score, right_score
 	if button.pin.number == 14:
-		print(left_name + ' won the game')
+		left_score += 1
+		print(left_name + ' won this round')
 	else:
-		print(right_name + ' won the game')
+		right_score += 1
+		print(right_name + ' won this round')
 
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
+
+while not (left_button.is_pressed or right_button.is_pressed):
+	sleep(0.1)
+
+right_button.when_pressed = None
+left_button.when_pressed = None
+
+print(f"{left_name}'s total score: {left_score}")
+print(f"{right_name}'s total score: {right_score}")
 exit()
